f97669b3c0ba130ba486c0cf423c048e2a058e1e
ansiace <animeshraj80707@gmail.comgit config --global user.email>
2024-07-12 11:33:48 +0530

    2ndarray code

---
+++ array2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[]={1,2,3,4,5,7,3};
+    // arr[5]=10;
+    int a,i;
+    a = sizeof (arr) / sizeof (arr[0]); 
+
+    for(i=0;i<a;i++){
+
+        arr[i]=arr[i]-arr[i];
+        cout<<arr[i]<<" ";
+
+    }
+
+    cout<<endl<<arr[5];
+    return 0;
+}
---
+++ array3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[]={1,2,3,4,5,7,3};
+    int a,i;
+    a = sizeof (arr) / sizeof (arr[0]); 
+    for(i=0;i<a;i++){
+
+        arr[i]=arr[i]-arr[i];
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
