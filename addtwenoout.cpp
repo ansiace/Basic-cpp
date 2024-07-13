886ba970f499d54bcf9e6332e1bde8bb3d1922d8
ansiace <animeshraj80707@gmail.comgit config --global user.email>
2024-07-13 05:52:11 +0530

    2nd day of push

---
+++ addtwenoout.cpp
@@ -0,0 +1,17 @@
+
+
+#include <iostream>
+ using namespace std;
+
+int main(){
+    int n;
+    cout<<"ente a number :";
+    cin>>n;
+    if((n/2)*2==n){
+        cout<<"number is even"<<endl;
+    }
+    else{
+        cout<<"number is odd";
+    }
+return 0;
+}
