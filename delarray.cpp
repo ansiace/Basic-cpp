bc245f8f73cd4ed1d7d5c53f0fd5df785f8fb1a0
ansiace <animeshraj80707@gmail.comgit config --global user.email>
2024-07-14 02:17:19 +0530

    delete array

---
+++ delarray.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,4,5,6};
+    int n = sizeof arr / sizeof arr[0];
+    int pos;
+    cin>>pos;
+    for(int i=pos;i<n;i++){
+        arr[i]=arr[i+1];
+
+    }
+    for( int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
