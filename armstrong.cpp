ced4707d149e3d0308ff0ce8d1e24e51f3fd6bd1
ansiace <animeshraj80707@gmail.comgit config --global user.email>
2024-07-14 01:51:27 +0530

    armstrong number

---
+++ armstrong.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,temp,sum=0,rem;
+    cin>>n;
+    temp=n;
+    while(temp!=0){
+        rem=temp%10;
+        sum=sum+(rem*rem*rem);
+        temp=temp/10;
+
+    }
+    if(n==sum){
+        cout<<"number is armstrong ";
+
+    }
+    else
+    {
+        cout<<"number is not armstrong ";
+    }
+}
