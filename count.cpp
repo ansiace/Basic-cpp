54f51a8c5eed2b0ca1901e0ed344c4ce1004f1d6
ansiace <animeshraj80707@gmail.comgit config --global user.email>
2024-07-14 01:59:29 +0530

    basic problems

---
+++ count.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num,sum=0;
+    cin>>num;
+    while(num!=0){
+        num=num/10;
+        sum++;
+    }
+    cout<<"total digit is "<<sum;
+}
---
+++ palindrome.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,c,b=0,r;
+    cout<<"enter the number"<<endl;
+    cin>>n;
+    c=n;
+    while(n!=0){
+        r=n%10;
+        b=r+(b*10);
+        n=n/10;
+    }
+    if (c==b){
+        cout<<"no is palindrome";
+    
+    }
+    else{
+        cout<<"no is not palindrome";
+    }
+    return 0;
+}
