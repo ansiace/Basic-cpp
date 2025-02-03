21a3180c450686bd81d6ac4d738a934a2628995a
Animesh Raj <animeshraj80707@gmail.com>
2025-02-03 20:12:51 +0530

    Update armstrong.cpp

--- armstrong.cpp
+++ armstrong.cpp
@@ -3,16 +3,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n,temp,sum=0,rem;
-    cin>>n;
-    temp=n;
+    int num ,temp,sum=0,rem;
+    cin>>num;
+    temp=num;
     while(temp!=0){
         rem=temp%10;
         sum=sum+(rem*rem*rem);
         temp=temp/10;
 
     }
-    if(n==sum){
+    if(num==sum){
         cout<<"number is armstrong ";
 
     }
