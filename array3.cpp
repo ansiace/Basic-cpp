be461e8f741a61349d2815a6019d86029c1e1d47
Animesh Raj <animeshraj80707@gmail.com>
2025-02-05 12:38:32 +0530

    Update array3.cpp

--- array3.cpp
+++ array3.cpp
@@ -5,8 +5,8 @@ int main(){
     int arr[]={1,2,3,4,5,7,3};
     int a,i;
     a = sizeof (arr) / sizeof (arr[0]); 
-    for(i=0;i<a;i++){
 
+    for( i = 0 ; i < a ; i++ ){
         arr[i]=arr[i]-arr[i];
         cout<<arr[i]<<" ";
     }
